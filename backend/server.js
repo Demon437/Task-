530e965ec8074f9cc5af92e4ee4247da29c36ab3
Demon437 <namdevshanu206@gmail.com>
2025-06-30 00:43:20 +0530

    fix: updated server.js to serve frontend build

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,13 @@ const taskRoutes = require("./routes/tasks");
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// Serve frontend only if in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-    });
-}
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 
 app.get("/api", (req, res) => {
     res.send("Welcome to the Task Manager API");
@@ -32,10 +31,7 @@ app.get("/api", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB Connected");
         app.listen(PORT, () => {
